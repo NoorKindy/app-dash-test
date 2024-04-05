ab34bd24b1f03549915f9321d6a6f9f0a2cb9194
NoorKindy <noorelkindy@gmail.com>
2024-04-06 01:37:34 +0200

    refresh all data14

--- src/pages/topic_tab.py
+++ src/pages/topic_tab.py
@@ -115,8 +115,7 @@ def generate_color():
 # print(flag)
 date = "20240317"
 
-#csv_file_path = f"src/csv_input/Topic_Modeling_Analysis_{date}.csv"
-csv_file_path = '/opt/render/project/src/csv_input/Topic_Modeling_Analysis_20240317.csv'
+csv_file_path = f"csv_input\\topic_modeling_{date}.csv"
 data = pd.read_csv(csv_file_path)
 #df_topic_data = load_and_structure_data(f"csv_input\\Topic_Modeling_Analysis_{date}.csv")
 #df_topic_data = pd.read_csv(csv_file_path)
